3d86e40d829693daf2da430abc882491867388a7
karussellbremser <kicker789@hotmail.de>
2015-06-07 21:16:55 -0400

    Problem 2 finished

--- pa3/problem2/markov.py
+++ pa3/problem2/markov.py
@@ -6,7 +6,8 @@ import random
 NPREF = 2
 NONWORD = "\n"
 MAXGEN = 10000
-statetab = {}  # dictionary
+statetab = {}  # dictionary for mapping prefixes to suffixes
+testset = set()  # set for testing
 prefix = collections.deque([NONWORD] * NPREF)  # deque
 
 # build
@@ -14,6 +15,7 @@ for line in sys.stdin:
     for word in line.split():
         # append word to suffix list or create list if not yet created; 'tuple' for "hashability" for dictionary
         statetab.setdefault(tuple(prefix), []).append(word)
+        testset.add(tuple(prefix))  # add prefix to test set so that its occurence can be tested later
         prefix.rotate(-1)  # rotate one left
         prefix[-1] = word  # overwrite last entry
 statetab.setdefault(tuple(prefix), []).append(NONWORD)
@@ -25,6 +27,9 @@ for i in range(MAXGEN):
     w = suf[random.randrange(0, len(suf))]
     if w == NONWORD:
         break
-    print(w, end=' ')
+    if not tuple(prefix) in testset:
+        print("Internal error: generated prefix not found in original data!")
+        break
+    print(w)
     prefix.rotate(-1)
     prefix[-1] = w
